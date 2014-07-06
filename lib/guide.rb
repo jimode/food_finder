60f2f30ab11634bf17e01cf6adf3e1ba996bda45
Jimi Odeyemi <jimode68@gmail.com>
2014-07-06 22:49:44 +0100

    restaurant class

--- lib/guide.rb
+++ lib/guide.rb
@@ -1,8 +1,18 @@
+require 'restaurant'
 class Guide
 	def initialize(path=nil)
 		# locate the restaurant text file at path
+		Restaurant.filepath = path
+		if Restaurant.file_exists?
+			puts "Found restaurant file."
 		# or create a new file
+		elsif Restaurant.create_file
+			puts "Created restaurant file"
 		# exit if create files
+		else
+			puts "Exiting.\n\n"
+			exit!
+		end
 	end
 	
 	def launch!
