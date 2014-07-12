00f472c2bca381e3fc44d837cd8168c99eb952bf
Jimi Odeyemi <jimode68@gmail.com>
2014-07-12 17:02:56 +0100

    handle input in loop

--- lib/guide.rb
+++ lib/guide.rb
@@ -17,11 +17,31 @@ class Guide
 	
 	def launch!
 		introduction
-		# action loop
-		#  what do you want to do? (list, find, add, quite)
-		# do that action
-		# repeat until user quits
+		# action loop\
+		result = nil
+		until result == :quit
+			#  what do you want to do? (list, find, add, quite)
+			print "> "
+			user_response = gets.chomp
+			# do that action
+			result = do_action(user_response)
+		end
 		conclusion
+	end
+
+	def do_action(action)
+		case action
+		when 'list'
+			puts "Listing ..."
+		when 'find'
+			puts "Finding ..."
+		when 'add'
+			puts "Adding ..."
+		when 'quit'
+			return :quit
+		else
+			puts "\nI don't understand that command.\n"
+		end
 	end
 
 	def introduction
